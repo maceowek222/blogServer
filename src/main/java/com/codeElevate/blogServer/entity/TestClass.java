3f0e6c4ea73c285e2a595f0652ca0cd8f0d754d9
Maciej Sławiński <maceowek@gmail.com>
2024-03-23 23:22:45 +0100

    Initial commit

---
+++ src/main/java/com/codeElevate/blogServer/entity/TestClass.java
@@ -0,0 +1,10 @@
+package com.codeElevate.blogServer.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class TestClass {
+    @Id
+    private int id;
+}
